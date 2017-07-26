874f0268158256a7922c72b6e6559972efc9eb9f
songzhifei <zhifeisong@163.com>
2017-07-26 22:21:18 +0800

    增加OutBackStoreService服务，增加超时未归还页面

---
+++ toolstrackingsystem/service.toolstrackingsystem/Interface/IOutBackStoreService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewEntity.toolstrackingsystem;
+
+namespace service.toolstrackingsystem
+{
+    public interface IOutBackStoreService
+    {
+        /// <summary>
+        /// 获取超时未归还的工具信息
+        /// </summary>
+        /// <param name="toolCode"></param>
+        /// <param name="personCode"></param>
+        /// <returns></returns>
+        List<NotBackToolEntity> GetNotBackToolInfoList(string toolCode, string personCode);
+    }
+}
