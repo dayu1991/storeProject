24c0348fe822d65f92a2d048fdceac718919206e
songzhifei <zhifeisong@163.com>
2017-07-26 21:55:58 +0800

    入库查询功能完成

---
+++ toolstrackingsystem/toolstrackingsystem/入库查询/FrmInStoreManage.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevComponents.DotNetBar;
+using log4net;
+using service.toolstrackingsystem;
+using ViewEntity.toolstrackingsystem;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using System.IO;
+
+namespace toolstrackingsystem
+{
+    public partial class FrmInStoreManage : Office2007RibbonForm
+    {
+        ILog logger = log4net.LogManager.GetLogger(typeof(FormLogin));
+        private ICurrentCountInfoService _CurrentCountInfoService;
+        private List<ToolInfoInStoreEntity> resultList = new List<ToolInfoInStoreEntity>();
+        public FrmInStoreManage()
+        {
+            this.EnableGlass = false;
+            InitializeComponent();
+        }
+
+        private void FrmInStoreManage_Load(object sender, EventArgs e)
+        {
+            this._CurrentCountInfoService = Program.container.Resolve<ICurrentCountInfoService>() as CurrentCountInfoService;
+        }
+
+        private void TollList_dataGridViewX_RowStateChanged(object sender, DataGridViewRowStateChangedEventArgs e)
+        {
+            e.Row.HeaderCell.Value = string.Format("{0}", e.Row.Index + 1);
+        }
+        private void Search_buttonX_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pagerControl1.OnPageChanged += new EventHandler(pagerControl1_OnPageChanged);
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                logger.ErrorFormat("具体位置={0},重要参数Message={1},StackTrace={2},Source={3}", "toolstrackingsystem--FrmInStoreManage--Search_buttonX_Click", ex.Message, ex.StackTrace, ex.Source);
+            }
+        }
+        private void LoadData()
+        {
+            string ToolCode = ToolCode_textBox.Text;
+            //数据总记录数
+            long Count;
+            //获取分页的数据
+            resultList = _CurrentCountInfoService.GetToolInfoInStoreList(ToolCode, pagerControl1.PageIndex, pagerControl1.PageSize, out Count);
+            TollList_dataGridViewX.DataSource = resultList;
+            pagerControl1.DrawControl(Convert.ToInt32(Count));
+            for (int i = 0; i < TollList_dataGridViewX.Columns.Count; i++)
+            {
+                TollList_dataGridViewX.Columns[i].SortMode = DataGridViewColumnSortMode.Programmatic;
+            }
+            TollList_dataGridViewX.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            TollList_dataGridViewX.Columns[0].HeaderText = "配属";
+            TollList_dataGridViewX.Columns[1].HeaderText = "类别";
+            TollList_dataGridViewX.Columns[2].HeaderText = "包编码";
+            TollList_dataGridViewX.Columns[3].HeaderText = "包名称";
+            TollList_dataGridViewX.Columns[4].HeaderText = "工具编码";
+            TollList_dataGridViewX.Columns[5].HeaderText = "工具名称";
+            TollList_dataGridViewX.Columns[6].HeaderText = "型号";
+            TollList_dataGridViewX.Columns[7].HeaderText = "位置";
+            TollList_dataGridViewX.Columns[8].HeaderText = "备注";
+            TollList_dataGridViewX.Columns[9].HeaderText = "入库时间";
+            TollList_dataGridViewX.Columns[10].HeaderText = "操作员";
+            pagerControl1.DrawControl(Convert.ToInt32(Count));
+        }
+
+        private void pagerControl1_OnPageChanged(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+        /// <summary>
+        /// 导出Excel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void export_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (resultList.Count <= 0)
+                {
+                    MessageBox.Show("数据为空，无法导出");
+                    return;
+                }
+                SaveFileDialog fileDialog = new SaveFileDialog();
+                fileDialog.Filter = "Excel(*.xls)|*.xls|Excel(*.xlsx)|*.xlsx";
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    NPOI.SS.UserModel.IWorkbook book = null;
+                    if (fileDialog.FilterIndex == 1)
+                    {
+                        book = new NPOI.HSSF.UserModel.HSSFWorkbook();
+                    }
+                    else
+                    {
+                        book = new NPOI.XSSF.UserModel.XSSFWorkbook();
+                    }
+                    NPOI.SS.UserModel.ISheet sheet = book.CreateSheet("入库查询列表");
+                    // 添加表头
+                    NPOI.SS.UserModel.IRow row = sheet.CreateRow(0);
+                    for (int i = 0; i < TollList_dataGridViewX.Columns.Count; i++)
+                    {
+                        var item = TollList_dataGridViewX.Columns[i];
+                        NPOI.SS.UserModel.ICell cell = row.CreateCell(i);
+                        cell.SetCellType(NPOI.SS.UserModel.CellType.String);
+                        cell.SetCellValue(item.HeaderText);
+                    }
+                    // 添加数据
+                    for (int i = 0; i < resultList.Count; i++)
+                    {
+                        var item = resultList[i];
+                        row = sheet.CreateRow(i + 1);
+                        NPOI.SS.UserModel.ICell cell = row.CreateCell(0);
+                        cell.SetCellType(NPOI.SS.UserModel.CellType.String);
+                        cell.SetCellValue(item.TypeName);
+                        cell = row.CreateCell(1);
+                        cell.SetCellType(NPOI.SS.UserModel.CellType.String);
+                        cell.SetCellValue(item.ChildTypeName);
+                        cell = row.CreateCell(2);
+                        cell.SetCellType(NPOI.SS.UserModel.CellType.String);
+                        cell.SetCellValue(item.PackCode);
+                        cell = row.CreateCell(3);
+                        cell.SetCellType(NPOI.SS.UserModel.CellType.String);
+                        cell.SetCellValue(item.PackName);
+                        cell = row.CreateCell(4);
+                        cell.SetCellType(NPOI.SS.UserModel.CellType.String);
+                        cell.SetCellValue(item.ToolCode);
+                        cell = row.CreateCell(5);
+                        cell.SetCellType(NPOI.SS.UserModel.CellType.String);
+                        cell.SetCellValue(item.ToolName);
+                        cell = row.CreateCell(6);
+                        cell.SetCellType(NPOI.SS.UserModel.CellType.String);
+                        cell.SetCellValue(item.Models);
+                        cell = row.CreateCell(7);
+                        cell.SetCellType(NPOI.SS.UserModel.CellType.String);
+                        cell.SetCellValue(item.Location);
+                        cell = row.CreateCell(8);
+                        cell.SetCellType(NPOI.SS.UserModel.CellType.String);
+                        cell.SetCellValue(item.Remarks);
+                        cell = row.CreateCell(9);
+                        cell.SetCellType(NPOI.SS.UserModel.CellType.String);
+                        cell.SetCellValue(item.InStoreTime);
+                        cell = row.CreateCell(10);
+                        cell.SetCellType(NPOI.SS.UserModel.CellType.String);
+                        cell.SetCellValue(item.OptionPerson);
+                    }
+                    // 写入 
+                    System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                    book.Write(ms);
+                    book = null;
+                    using (FileStream fs = new FileStream(fileDialog.FileName, FileMode.Create, FileAccess.Write))
+                    {
+                        byte[] data = ms.ToArray();
+                        fs.Write(data, 0, data.Length);
+                        fs.Flush();
+                    }
+                    ms.Close();
+                    ms.Dispose();
+                    MessageBox.Show("导出成功");
+                    return;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                logger.ErrorFormat("具体位置={0},重要参数Message={1},StackTrace={2},Source={3}", "toolstrackingsystem--FrmInStoreManage--export_button_Click", ex.Message, ex.StackTrace, ex.Source);
+            }
+
+        }
+        private void Print_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string ToolCode = ToolCode_textBox.Text;
+                FrmPrintToolInStore printFrm = new FrmPrintToolInStore();
+                printFrm.Tag = ToolCode;
+                printFrm.ShowDialog();
+            }
+            catch (Exception ex)
+            {
+                logger.ErrorFormat("具体位置={0},重要参数Message={1},StackTrace={2},Source={3}", "toolstrackingsystem--FrmCurrentCountManage--export_button_Click", ex.Message, ex.StackTrace, ex.Source);
+            }
+        }
+    }
+}
---
+++ toolstrackingsystem/toolstrackingsystem/入库查询/FrmPrintToolInStore.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace toolstrackingsystem
+{
+    partial class FrmPrintToolInStore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.cangku_manage_dbDataSet = new toolstrackingsystem.cangku_manage_dbDataSet();
+            this.t_ToolInfoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.t_ToolInfoTableAdapter = new toolstrackingsystem.cangku_manage_dbDataSetTableAdapters.t_ToolInfoTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.cangku_manage_dbDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t_ToolInfoBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource2.Name = "DataSet1";
+            reportDataSource2.Value = this.t_ToolInfoBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource2);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "toolstrackingsystem.入库查询.Report4.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(723, 351);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // cangku_manage_dbDataSet
+            // 
+            this.cangku_manage_dbDataSet.DataSetName = "cangku_manage_dbDataSet";
+            this.cangku_manage_dbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // t_ToolInfoBindingSource
+            // 
+            this.t_ToolInfoBindingSource.DataMember = "t_ToolInfo";
+            this.t_ToolInfoBindingSource.DataSource = this.cangku_manage_dbDataSet;
+            // 
+            // t_ToolInfoTableAdapter
+            // 
+            this.t_ToolInfoTableAdapter.ClearBeforeFill = true;
+            // 
+            // FrmPrintToolInStore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 351);
+            this.Controls.Add(this.reportViewer1);
+            this.DoubleBuffered = true;
+            this.Name = "FrmPrintToolInStore";
+            this.Text = "打印预览";
+            this.Load += new System.EventHandler(this.FrmPrintToolInStore_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cangku_manage_dbDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.t_ToolInfoBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource t_ToolInfoBindingSource;
+        private cangku_manage_dbDataSet cangku_manage_dbDataSet;
+        private cangku_manage_dbDataSetTableAdapters.t_ToolInfoTableAdapter t_ToolInfoTableAdapter;
+    }
+}
